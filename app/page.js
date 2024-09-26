601c21dc7cb7d4cab7cc939104c8f65e68c84d00
vivek1842 <vivekghule2002@gmail.com>
2024-09-26 23:12:28 +0530

    shadcn ui button added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div><h2>Hello</h2></div>
+    <div>
+      <h2>ShadCn Btn</h2>
+      <Button variant="outline">Submit</Button>
+    </div>
   );
 }
